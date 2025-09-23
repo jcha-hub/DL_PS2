16f80b4fc5fc9b6015c300192fef34fbb51abeb7
PottyPrincess1 <136873959+PottyPrincess1@users.noreply.github.com>
2025-09-23 19:46:40 -0400

    y

--- part2-pytorch/models/my_model.py
+++ part2-pytorch/models/my_model.py
@@ -41,10 +41,11 @@ class MyModel(nn.Module):
         self.conv1 = nn.Conv2d(3,96,7, 1, padding=2)
         self.conv2 = nn.Conv2d(96, 96, 5, 1, padding=1)
         self.conv3 = nn.Conv2d(96, 128, 5, 1, padding='same')
-        self.conv4 = nn.Conv2d(128,164,3, 1, padding='same')
-        self.conv5 = nn.Conv2d(164, 196, 3, 1, padding=0)
+        self.conv4 = nn.Conv2d(128,160,3, 1, padding='same')
+        self.conv5 = nn.Conv2d(160, 192, 3, 1, padding='same')
+        self.conv6 = nn.Conv2d(192, 224, 3, 1, padding=0)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(in_features = 3136, out_features = 500)
+        self.fc1 = nn.Linear(in_features = 896, out_features = 500)
         self.fc2 = nn.Linear(500, 80)
         self.fc3 = nn.Linear(80,10)
 
@@ -54,16 +55,17 @@ class MyModel(nn.Module):
         x = self.pool(F.relu(self.conv3(x)))
         x = F.relu(self.conv4(x))
         x = F.relu(self.conv5(x))
-        x = F.dropout(x, 0.25)
+        x = self.pool(F.relu(self.conv6(x)))
+        x = F.dropout(x, 0.3)
 
         # # # fc_in_features = x.numel() // x.shape[0] - comment out fc to get input into fc
         # print('hello-----------------------')
         # fc_in_features = x.numel() // x.shape[0]
-        # print('fc_in_features', fc_in_features)       #3136
+        # print('fc_in_features', fc_in_features)       #896
 
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, 0.25)   #reduce overfitting
+        x = F.dropout(x, 0.5)   #reduce overfitting
 
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
